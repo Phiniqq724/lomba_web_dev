e5612244963b084f33ebfcbfc76c7a9c55238330
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2023-12-21 22:56:24 +0700

    Co-authored-by: Haza Nasrullah <hazz01@users.noreply.github.com> Co-authored-by: Phiniqq724 <Phiniqq724@users.noreply.github.com> section 2

--- script/script.js
+++ script/script.js
@@ -2,18 +2,18 @@
 
 const heroElement = document.querySelector("header.hero");
 const heroObserver = new IntersectionObserver(
-    (entries) => {
-        const [entry] = entries;
+  (entries) => {
+    const [entry] = entries;
 
-        if(entry.isIntersecting) {
-            document.querySelector("nav").classList.add("scrolled");
-        } else {
-            document.querySelector("nav").classList.remove("scrolled")
-        }
-    }, {threshold: 0.9}
-)
+    if (entry.isIntersecting) {
+      document.querySelector("nav").classList.add("scrolled");
+    } else {
+      //   document.querySelector("nav").classList.remove("scrolled");
+    }
+  },
+  { threshold: 1.1 }
+);
 
 heroObserver.observe(heroElement);
-
 
 // Form Validation
