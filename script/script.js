5146f8535a18b3ffb660a555cc7ffb089af0ade4
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2023-12-22 22:26:46 +0700

    Co-authored-by: Phiniqq724 <Phiniqq724@users.noreply.github.com> Co-authored-by: Haza Nasrullah <hazz01@users.noreply.github.com>

--- script/script.js
+++ script/script.js
@@ -12,5 +12,49 @@ function scrollFunction() {
 }
 
 // logic dark mode
+const checkbox = document.querySelector("#toggle");
+const html = document.querySelector("html");
+
+checkbox.addEventListener("click", function () {
+  checkbox.checked ? html.setAttribute("data-bs-theme", "dark") : html.removeAttribute("data-bs-theme", "dark");
+});
 
 // Form Validation
+//ok
+
+// slider
+$(".slider").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
