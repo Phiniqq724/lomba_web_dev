d377885d8d960447465fbc9e7276b77920b79ded
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2023-12-24 13:56:40 +0700

    Co-authored-by: Haza Nasrullah <hazz01@users.noreply.github.com> Co-authored-by: Phiniqq724 <Phiniqq724@users.noreply.github.com>

--- script/script.js
+++ script/script.js
@@ -14,9 +14,25 @@ function scrollFunction() {
 // logic dark mode
 const checkbox = document.querySelector("#toggle");
 const html = document.querySelector("html");
+const navnav = document.getElementById("navnav");
+const p = document.querySelectorAll("p");
+const jumbotron = document.querySelector(".jumbotron");
+const backdrops = document.querySelector("div.jumbotron div#backdrops");
+const garis = document.getElementById("garis");
+const display1 = document.querySelector(".display-1");
 
 checkbox.addEventListener("click", function () {
   checkbox.checked ? html.setAttribute("data-bs-theme", "dark") : html.removeAttribute("data-bs-theme", "dark");
+  checkbox.checked ? navnav.classList.add("bg-dark") : navnav.classList.remove("bg-dark");
+  checkbox.checked ? p.setAttribute("class", "text-white") : p.removeAttribute("class", "text-white");
+});
+
+checkbox.addEventListener("click", function () {
+  checkbox.checked ? backdrops.classList.add("jumbotron-backdrop") : backdrops.classList.remove("jumbotron-backdrop");
+  checkbox.checked ? garis.classList.add("border-white") : garis.classList.add("border-black") || garis.classList.remove("border-white");
+});
+checkbox.addEventListener("click", function () {
+  checkbox.checked ? display1.classList.add("shadow-whites") : display1.classList.remove("shadow-whites");
 });
 
 // Form Validation
